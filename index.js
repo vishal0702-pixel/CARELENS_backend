021f350b20ce8def036cee15d0255ef0f63af9b3
vishal k <vishalcodex07@gmail.com>
2026-04-01 13:59:07 +0530

    removed env file from repository

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const intializeconnection = async()=>{
    try{  
       await Promise.all([main(),redisclient.connect()]);
       console.log("both database connected ");
-         app.listen( 3000, ()=>{
+         app.listen( process.env.PORT, ()=>{
             console.log("databse connected and server listen  suceesfully at 6000")
        
       })
